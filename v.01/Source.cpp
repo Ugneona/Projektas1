200944b20c4db3cd4ef6dc65b15def2f19b65647
Ugnė Kniukštaitė <90412167+Ugneona@users.noreply.github.com>
2021-09-24 15:20:16 +0300

    Antras atnaujinimas su vektoriais
    Su vektoriais, kai n is anksto nera zinomas

--- v.01/Source.cpp
+++ v.01/Source.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 
 using std::cin;
@@ -14,18 +15,21 @@ using std::left;
 using std::fixed;
 using std::setprecision;
 using std::sort;
+using std::vector;
 
 
 struct studentas
 {
     string vardas, pavarde;
-    double nd[10]{};
-    int egz;
+    vector <double> nd;
+    double egz;
     double galutinis;
     double mediana;
 };
 
-void pild(studentas& s, int& ndSk1, int& ii);
+void vardPavard(studentas& s, int& ii);
+void pild(studentas& s);
+void pildnd(studentas& s, int& ii, int& ndSk1);
 void spausdinti(studentas& s);
 bool check_string(string& str);
 double med(studentas& s, int& ndSk1);
@@ -35,6 +39,7 @@ int main()
 {
     int studentSk, ndSk; 
     studentas grupe[10];
+    char ats;
     cout << "Iveskite studentu skaiciu: " << endl;
     cin >> studentSk;
     while (studentSk <= 0 || cin.fail())
@@ -44,20 +49,31 @@ int main()
         cin.ignore(256, '\n');
         cin >> studentSk;
     }
-
-    cout << "Iveskite, kiek bus namu darbu (1-10)" << endl;
-    cin >> ndSk; 
-    while (cin.fail() || ndSk < 1 || ndSk > 10)
+    cout << "Ar namu darbu skaicius yra zinomas is anksto? t- jei taip, n- jei ne" << endl;
+    cin >> ats;
+    if (ats == 't' || ats == 'T')
     {
-        cin.clear();
-        cout << "Namu darbu skaicius yra netinkamas, iveskite skaiciu didesni uz 0" << endl;
-        cin.ignore(256, '\n');
+        cout << "Iveskite, kiek bus namu darbu (1-10)" << endl;
         cin >> ndSk;
+        while (cin.fail() || ndSk < 1 || ndSk > 10)
+        {
+            cin.clear();
+            cout << "Namu darbu skaicius yra netinkamas, iveskite skaiciu didesni uz 0" << endl;
+            cin.ignore(256, '\n');
+            cin >> ndSk;
+        }
+        for (int i = 0; i < studentSk; i++)
+        {
+            vardPavard(grupe[i], i);
+            pildnd(grupe[i], i, ndSk);
+        }
     }
- 
-    for (int i = 0; i < studentSk; i++)
-    {
-        pild(grupe[i], ndSk, i);
+    else {
+        for (int i = 0; i < studentSk; i++)
+        {
+            vardPavard(grupe[i], i);
+            pild(grupe[i]);
+        }
     }
     cout << setw(20) << left << "Pavarde" << setw(20) << left << "Vardas" << setw(20) << left << "Galutinis (Vid.)" << left << "Galutinis (Mediana)" << endl;
     cout << "-------------------------------------------------------------------------" << endl;
@@ -68,9 +84,8 @@ int main()
 
     return 0;
 }
-void pild(studentas& s, int& ndSk1, int& ii)
+void vardPavard(studentas& s, int& ii)
 {
-    float sum = 0.;
     cout << "Iveskite " << ii + 1 << " studento varda: " << endl;
     cin >> s.vardas;
     while (check_string(s.vardas) == false)
@@ -89,20 +104,42 @@ void pild(studentas& s, int& ndSk1, int& ii)
         cin.ignore(256, '\n');
         cin >> s.pavarde;
     }
-
+}
+void pild(studentas& s)
+{
+    double sum = 0.;
+    double paz;
+    char ats;
     cout << "Iveskite gautus pazymius is namu darbu (1-10): " << endl;
-    for (int j = 0; j < ndSk1; j++)
+    cin >> paz;
+    while (cin.fail() || paz < 1 || paz > 10)
     {
-        cin >> s.nd[j];
-        while (cin.fail() || s.nd[j] < 1 || s.nd[j] > 10)
+        cin.clear();
+        cout << "Neteisingai ivedete pazymi, prasome dar karta ivesti (1-10)" << endl;
+        cin.ignore(256, '\n');
+        cin >> paz;
+    }
+    s.nd.push_back(paz);
+    sum += paz;
+    cout << "Ar dar bus pazymiu is namu darbu? t - jei bus dar, n - jei nebebus" << endl;
+    cin >> ats;
+    while (ats == 'T' || ats == 't')
+    {
+        cout << "Iveskite sekanti pazymi is namu darbu" << endl;
+        cin >> paz;
+        while (cin.fail() || paz < 1 || paz > 10)
         {
             cin.clear();
             cout << "Neteisingai ivedete pazymi, prasome dar karta ivesti (1-10)" << endl;
             cin.ignore(256, '\n');
-            cin >> s.nd[j];
+            cin >> paz;
         }
-        sum += s.nd[j];
+        s.nd.push_back(paz);
+        sum += paz;
+        cout << "Ar dar bus pazymiu is namu darbu? t - jei bus dar, n - jei nebebus" << endl;
+        cin >> ats;
     }
+    
     cout << "Iveskite egzamino pazymi (1-10): " << endl;
     cin >> s.egz;
     while (cin.fail() || s.egz <= 0 || s.egz > 10)
@@ -112,8 +149,9 @@ void pild(studentas& s, int& ndSk1, int& ii)
         cin.ignore(256, '\n');
         cin >> s.egz;
     }
-    s.galutinis = 0.6 * s.egz + 0.4 * (sum / ndSk1);
-    s.mediana = med(s, ndSk1);
+    s.galutinis = 0.6 * s.egz + 0.4 * (sum / s.nd.size());
+    int dydis = s.nd.size();
+    s.mediana = med(s, dydis);
 }
 void spausdinti(studentas& s)
 {
@@ -132,9 +170,43 @@ bool check_string(string& str)
 
 double med(studentas& s, int& ndSk1)
 {
-    sort(s.nd, s.nd + ndSk1);
-    int vid = ndSk1 / 2;
-    if (ndSk1 % 2 == 0) return (double)(s.nd[vid - 1] + s.nd[vid]) / 2.0;
-    else return (double)s.nd[vid];
+    sort(s.nd.begin(), s.nd.end());
+    if (ndSk1 % 2 == 0) return (double)(s.nd[(ndSk1/2) - 1] + s.nd[ndSk1/2]) / 2.0;
+    else return (double)s.nd[ndSk1/2];
 }
+
+void pildnd(studentas& s, int& ii, int& ndSk1)
+{
+    double sum = 0.;
+    double paz;
+
+    cout << "Iveskite gautus pazymius is namu darbu (1-10): " << endl;
+    for (int j = 0; j < ndSk1; j++)
+    {
+        cin >> paz;
+        while (cin.fail() || paz < 1 || paz > 10)
+        {
+            cin.clear();
+            cout << "Neteisingai ivedete pazymi, prasome dar karta ivesti (1-10)" << endl;
+            cin.ignore(256, '\n');
+            cin >> paz;
+        }
+        s.nd.push_back(paz);
+        sum += paz;
+    }
+    cout << "Iveskite egzamino pazymi (1-10): " << endl;
+    cin >> s.egz;
+    while (cin.fail() || s.egz <= 0 || s.egz > 10)
+    {
+        cin.clear();
+        cout << "Neteisingai ivedete egzamino pazymi, prasome dar karta ivesti (1-10)" << endl;
+        cin.ignore(256, '\n');
+        cin >> s.egz;
+    }
+    s.galutinis = 0.6 * s.egz + 0.4 * (sum / ndSk1);
+    s.mediana = med(s, ndSk1);
+}
+
+
+
 
