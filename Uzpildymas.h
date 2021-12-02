3743ebd494b48bd9bb88ec720cf8d003cc96dfd3
Ugnė Kniukštaitė <90412167+Ugneona@users.noreply.github.com>
2021-12-02 21:25:15 +0200

    Delete Uzpildymas.h

--- Uzpildymas.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Studentas.h"
-#include "Uzklausos.h"
-#include "Patikrinimas.h"
-#include "Skaiciavimai.h"
-
-void studentoUzpildymasVardPavardNdEgz(int& studentuSkaicius, studentas(&grupe)[100]);
-void studentoUzpildymasRnd(int& studentuSkaicius, studentas(&grupe)[100]);
