7c6e30357a96b2715633ae2005e556c24d6845fe
Ugnė Kniukštaitė <90412167+Ugneona@users.noreply.github.com>
2021-12-02 21:11:25 +0200

    Delete Spausdinimas.h

--- v.03_bandymas/Spausdinimas.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Studentas.h"
-
-
-void spausdinti_vector(char& atsakymas, vector <studentas> grupe1);
-void spausdinti_struct(char& atsakymas, studentas(&grupe)[100], int& studentuSkaicius);
